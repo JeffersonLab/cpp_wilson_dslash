db09fd445d8201947b14e13f0858eeb45755a741
bjoo <bjoo>
2008-09-15 18:01:35 +0000

    Oops

--- include/cpp_dslash_types.h
+++ include/cpp_dslash_types.h
@@ -1,17 +1,15 @@
-#ifndef CPP_DSLASH_SCALAR_TYPES_H
-#define CPP_DSLASH_SCALAR_TYPES_H
+#ifndef CPP_DSLASH_TYPES_H
+#define CPP_DSLASH_TYPES_H
 
 namespace CPlusPlusWilsonDslash {
 
-
-
-  namespace DslashScalar32BitTypes {
+  namespace Dslash32BitTypes {
    typedef float FourSpinor[4][3][2];
    typedef float HalfSpinor[3][2][2];
    typedef float GaugeMatrix[3][3][2];
   }
 
-  namespace DslashScalar64BitTypes {
+  namespace Dslash64BitTypes {
    typedef double FourSpinor[4][3][2];
    typedef double HalfSpinor[2][3][2];
    typedef double GaugeMatrix[3][3][2];
